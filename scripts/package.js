c65c43703d486149f54810bfeb959ed7a537d77f
Power-Maverick <danish.naglekar@hotmail.com>
2025-11-04 14:47:11 -0500

    Refactor package script to use custom packaging script for cross-platform builds

---
+++ scripts/package.js
@@ -0,0 +1,29 @@
+#!/usr/bin/env node
+
+const { execSync } = require("child_process");
+const os = require("os");
+
+function run(cmd) {
+    console.log(`\n> ${cmd}\n`);
+    execSync(cmd, { stdio: "inherit" });
+}
+
+const platform = os.platform();
+
+switch (platform) {
+    case "darwin": // macOS
+        run("electron-builder --mac dmg --config.mac.identity=null --config.dmg.sign=false");
+        break;
+
+    case "win32": // Windows
+        run("electron-builder --win nsis");
+        break;
+
+    case "linux": // Linux
+        run("electron-builder --linux AppImage");
+        break;
+
+    default:
+        console.error(`❌ Unsupported platform: ${platform}`);
+        process.exit(1);
+}
